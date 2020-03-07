9479a1e7f7be7984161452716805091e2bbfc080
r2dev2bb8 <ronak.badhe@gmail.com>
2020-03-06 18:15:59 -0800

    finished setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def install(name: str) -> None:
     subprocess.call([sys.executable, '-m', 'pip', 'install', name])
 
 def youtube_download(url: str) -> None:
-    subprocess.call([sys.executable, '-m', 'youtube-dl', '--extract-audio', '--audio-format=mp3', url])
+    subprocess.call([sys.executable, '-m', 'youtube_dl', '--extract-audio', '--audio-format=mp3', url])
 
 # Create Music folder
 try:
